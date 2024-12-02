cfeca54ba74f6b0319f801fcdb5f1cd35c16bb65
Daniel Montes <gdanielmontesh9@gmail.com>
2024-12-01 21:05:54 -0600

    feat: :lipstick: adding buttons animations

--- angular-simple-counter/src/app/app.component.ts
+++ angular-simple-counter/src/app/app.component.ts
@@ -1,25 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   counter: number = 0;
+  isAnimatingIncrease = false;
+  isAnimatingReset = false;
+  isAnimatingDecrease = false;
 
   increase() {
     this.counter++;
-  }
-
-  decrease() {
-    this.counter--;
+    this.triggerAnimationIncrease();
   }
 
   reset() {
     this.counter = 0;
+    this.triggerAnimationReset();
+  }
+
+  decrease() {
+    this.counter--;
+    this.triggerAnimationDecrease();
+  }
+
+  triggerAnimationIncrease() {
+    this.isAnimatingIncrease = true;
+    setTimeout(() => {
+      this.isAnimatingIncrease = false;
+    }, 100);
+  }
+
+  triggerAnimationReset() {
+    this.isAnimatingReset = true;
+    setTimeout(() => {
+      this.isAnimatingReset = false;
+    }, 100);
+  }
+
+  triggerAnimationDecrease() {
+    this.isAnimatingDecrease = true;
+    setTimeout(() => {
+      this.isAnimatingDecrease = false;
+    }, 100);
   }
 }
