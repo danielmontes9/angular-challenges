7130bbf66473cadd6442a178cb3aee1e5fdae06a
Daniel Montes <gdanielmontesh9@gmail.com>
2024-12-03 23:56:58 -0600

    feat: :sparkles: loader on page start

--- angular-simple-counter/src/app/app.component.ts
+++ angular-simple-counter/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, CommonModule, LoaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  isLoading = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  }
+}
