5d81672380f387df6c3124b1dbe6f2378c7e16a8
Daniel Montes <gdanielmontesh9@gmail.com>
2025-04-17 13:55:27 -0600

    fix: :bug: import carbon styles

--- angular-forms-carbon/src/app/app.component.ts
+++ angular-forms-carbon/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { IconModule, UIShellModule } from "carbon-components-angular";
 
 @Component({
 	selector: "app-root",
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, UIShellModule, IconModule],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
