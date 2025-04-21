e30984e4dff53238c7f67fa2a6e79f83ec946825
Daniel Montes <gdanielmontesh9@gmail.com>
2025-04-20 20:30:46 -0600

    feat: :sparkles: add carbon module with routing

---
+++ angular-forms-carbon/src/app/carbon/carbon.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { CarbonRoutingModule } from "./carbon-routing.module";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+@NgModule({
+	declarations: [DashboardComponent],
+	imports: [CommonModule, CarbonRoutingModule],
+})
+export class CarbonModule {}
