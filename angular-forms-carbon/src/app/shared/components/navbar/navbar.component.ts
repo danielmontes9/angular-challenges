ccc2afa652cddd0de378c0d62f6d8ee39f54414c
Daniel Montes <gdanielmontesh9@gmail.com>
2025-04-20 20:14:31 -0600

    fix: :lipstick: icons in navbar

--- angular-forms-carbon/src/app/shared/components/navbar/navbar.component.ts
+++ angular-forms-carbon/src/app/shared/components/navbar/navbar.component.ts
@@ -4,6 +4,9 @@ import { IconService } from "carbon-components-angular";
 import Notification20 from "@carbon/icons/es/notification/20";
 import UserAvatar20 from "@carbon/icons/es/user--avatar/20";
 import Switcher20 from "@carbon/icons/es/switcher/20";
+import Awake20 from "@carbon/icons/es/awake/20";
+import Asleep20 from "@carbon/icons/es/asleep/20";
+import Help20 from "@carbon/icons/es/help/20";
 
 @Component({
 	selector: "app-navbar",
@@ -12,9 +15,22 @@ import Switcher20 from "@carbon/icons/es/switcher/20";
 	styleUrl: "./navbar.component.scss",
 })
 export class NavbarComponent {
+	currentTheme: string = "light";
+
 	constructor(protected iconService: IconService) {}
 
-	ngOnInit() {
-		this.iconService.registerAll([Notification20, UserAvatar20, Switcher20]);
+	ngOnInit(): void {
+		this.iconService.registerAll([
+			Notification20,
+			UserAvatar20,
+			Switcher20,
+			Awake20,
+			Asleep20,
+			Help20,
+		]);
+	}
+
+	activeDarkMode(event: boolean): void {
+		event == true ? (this.currentTheme = "dark") : (this.currentTheme = "light");
 	}
 }
