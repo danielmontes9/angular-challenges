b257cd74d73d8d04630080be1572876ce593619c
Daniel Montes <gdanielmontesh9@gmail.com>
2025-04-26 11:03:00 -0600

    fix: :bug: add menu buttons in navbar

--- angular-forms-carbon/src/app/shared/shared.module.ts
+++ angular-forms-carbon/src/app/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { IconModule, UIShellModule, ThemeModule } from "carbon-components-angular";
+import {
+	IconModule,
+	UIShellModule,
+	ThemeModule,
+	MenuButtonModule,
+	DropdownModule,
+} from "carbon-components-angular";
 
 import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @NgModule({
 	declarations: [NavbarComponent],
-	imports: [CommonModule, ThemeModule, UIShellModule, IconModule],
+	imports: [
+		CommonModule,
+		ThemeModule,
+		UIShellModule,
+		IconModule,
+		MenuButtonModule,
+		DropdownModule,
+	],
 	exports: [ThemeModule, NavbarComponent],
 })
 export class SharedModule {}
