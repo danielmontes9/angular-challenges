802b64f489b315657191410ad91d7b25ac35d8fd
Daniel Montes <gdanielmontesh9@gmail.com>
2025-05-03 03:27:00 -0600

    fix: :bug: background color

--- angular-forms-carbon/src/app/carbon/core/services/theme.service.ts
+++ angular-forms-carbon/src/app/carbon/core/services/theme.service.ts
@@ -15,5 +15,16 @@ export class ThemeService {
 	toggleTheme(theme: "white" | "g10" | "g90" | "g100"): void {
 		this.currentTheme.next(theme);
 		this._localStorageService.saveTheme("theme", theme);
+		this.updateDocumentBackground(theme);
+	}
+
+	private updateDocumentBackground(theme: "white" | "g10" | "g90" | "g100"): void {
+		const themeColors: { [key: string]: string } = {
+			white: "#ffffff",
+			g10: "#f4f4f4",
+			g90: "#262626",
+			g100: "#161616",
+		};
+		document.body.style.backgroundColor = themeColors[theme];
 	}
 }
