4188d2619d008ce6a936748518ab391eded49801
Daniel Montes <gdanielmontesh9@gmail.com>
2025-05-08 14:09:13 -0600

    feat: :sparkles: set translator service in root

--- angular-todo-ngzorro/src/app/app.module.ts
+++ angular-todo-ngzorro/src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './modules/todo/todo.component';
 import { SharedModule } from './shared/shared.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// Function to load translation files
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -14,7 +22,14 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
